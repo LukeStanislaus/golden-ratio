a91f1744bca9cdef461917f130ba28fe61316d1a
Luke Stanislaus <luke@stanislaus.co.uk>
2019-03-05 18:50:28 +0000

    d.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import init from "./logic";
-init(<HTMLCanvasElement>document.getElementById("root"), 1, 5, 1.6803,1);
+//import init from "./logic";
+//init(<HTMLCanvasElement>document.getElementById("root"), 1, 5, 1.6803,1);
 if (module.hot) {
   console.log("module is hot");
   module.hot.accept(["./logic.ts"], function () {
