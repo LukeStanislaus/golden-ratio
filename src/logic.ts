dd91bbf7339f86513bd50c81bd53babbba404f3e
Luke Stanislaus <luke@stanislaus.co.uk>
2019-03-06 17:17:45 +0000

    exports

--- src/logic.ts
+++ src/logic.ts
@@ -1,9 +1,12 @@
 let time = 0;
+exports.goldenratio= function(root: HTMLCanvasElement, speed:number=1, circles:number=10, golden_ratio:number=1.6803, circle_size:number=1, colours: boolean = false) {
+  setInterval(()=> time += speed, 1);
+  window.requestAnimationFrame(()=>{draw(root.getContext("2d"), (root.width>root.height?root.height:root.width)/(2*circle_size), (root.width>root.height?root.height:root.width)/2, circles, golden_ratio, colours)});
+}
 export function goldenratio(root: HTMLCanvasElement, speed:number=1, circles:number=10, golden_ratio:number=1.6803, circle_size:number=1, colours: boolean = false) {
   setInterval(()=> time += speed, 1);
   window.requestAnimationFrame(()=>{draw(root.getContext("2d"), (root.width>root.height?root.height:root.width)/(2*circle_size), (root.width>root.height?root.height:root.width)/2, circles, golden_ratio, colours)});
 }
-
 function draw(ctx: CanvasRenderingContext2D, radius:number, size:number, circles:number, golden_ratio:number, colours:boolean) { 
   ctx.clearRect(0, 0, size*2, size*2); // clear canvas
   let centre:[number,number]= [size,size];
